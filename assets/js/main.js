63bb25a945453ef26407f8f7a57790da92999833
aktivgo <AktivGo01062001@gmail.com>
2021-10-17 13:48:49 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,92 @@
+/*
+    Authorization
+ */
+
+$('button[id = "login-btn"]').click(function (e){
+    e.preventDefault();
+
+    $(`input`).removeClass('error');
+
+    let login = $('input[name = "login"]').val(),
+        password = $('input[name = "password"]').val();
+
+    $.ajax({
+        url: 'includes/signin.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            login: login,
+            password: password
+        },
+        success(data) {
+            if(data.status){
+                document.location.href = '../../profile'
+            } else{
+
+                if(data.type === 1){
+                    data.fields.forEach(function (field) {
+                        $(`input[name="${field}"]`).addClass('error');
+                    });
+                }
+
+                $('.message').removeClass('none').text(data.message);
+            }
+        }
+    });
+});
+
+/*
+    Getting avatar from filed
+ */
+
+let avatar = false;
+
+$('input[name="avatar"]').change(function (e){
+    avatar = e.target.files[0];
+});
+
+/*
+    Registration
+ */
+
+$('button[id = "register-btn"]').click(function (e){
+    e.preventDefault();
+
+    $(`input`).removeClass('error');
+
+    let fullName = $('input[name = "fullName"]').val(),
+        login = $('input[name = "login"]').val(),
+        email = $('input[name = "email"]').val(),
+        password = $('input[name = "password"]').val(),
+        passwordConfirm = $('input[name = "passwordConfirm"]').val();
+
+    let formData = new FormData();
+    formData.append('fullName', fullName);
+    formData.append('login', login);
+    formData.append('email', email);
+    formData.append('avatar', avatar);
+    formData.append('password', password);
+    formData.append('passwordConfirm', passwordConfirm);
+
+    $.ajax({
+        url: 'includes/signup.php',
+        type: 'POST',
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        cache: false,
+        data: formData,
+        success(data) {
+            if(data.status){
+                document.location.href = '../../authorization'
+            } else{
+                if(data.type === 1){
+                    data.fields.forEach(function (field) {
+                        $(`input[name="${field}"]`).addClass('error');
+                    });
+                }
+                $('.message').removeClass('none').text(data.message);
+            }
+        }
+    });
+});
---
+++ templates/index-template.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Регистрация и авторизация</title>
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="../assets/js/main.js"></script>
+</head>
+<body>
+
+<!-- Authorization or Registration Form -->
+
+<form>
+    <button type="submit" formaction="/authorization.php">Авторизация</button>
+    <label for="" style="align-self: center">или</label>
+    <button type="submit" formaction="../registration.php">Регистрация</button>
+</form>
+
+</body>
+</html>
